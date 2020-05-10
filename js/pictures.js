a48700de6d68d94bdaaac284e7930e8c32d53fd1
Rostyslav Miniukov <thresh1337@gmail.com>
2020-05-10 12:55:41 +0300

    Adding big picture view

--- js/pictures.js
+++ js/pictures.js
@@ -1,16 +1,20 @@
 'use strict';
 
+// Задаем константы
 var NUMBER_OF_PHOTOS = 25;
+var NUMBER_OF_AVATARS = 6;
 var MIN_LIKES = 15;
 var MAX_LIKES = 200;
 var PHOTO_FOLDER = 'photos/';
 var PHOTO_EXT = '.jpg';
+var AVATAR_FOLDER = 'img/avatar-';
+var AVATER_EXT = '.svg';
 var MIN_PHRASES = 1;
 var MAX_PHRASES = 2;
 var MAX_COMMENTS = 5;
 
 // Массивы с данными для фото
-var photoData = {
+var rawData = {
   comments: [
     'Всё отлично!',
     'В целом всё неплохо. Но не всё.',
@@ -27,6 +31,16 @@ var photoData = {
     'Отдыхаем...',
     'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами...',
     'Вот это тачка!'
+  ],
+
+  names: [
+    'Алла',
+    'Вадим',
+    'Коля',
+    'Саша',
+    'Влад',
+    'Дима',
+    'Маша'
   ]
 };
 
@@ -66,7 +80,7 @@ var setPhotoElementData = function (arrLength) {
       url: PHOTO_FOLDER + photoUrls[i] + PHOTO_EXT,
       likes: getRandomNumber(MIN_LIKES, MAX_LIKES + 1),
       comments: setComments(),
-      description: photoData.description[getRandomNumber(0, photoData.description.length)]
+      description: rawData.description[getRandomNumber(0, rawData.description.length)]
     });
   }
 
@@ -77,24 +91,31 @@ var setPhotoElementData = function (arrLength) {
 var setComments = function () {
   var comments = [];
   var numberOfComments = getRandomNumber(1, MAX_COMMENTS + 1);
+  var avatars = getPhotoUrl(NUMBER_OF_AVATARS);
 
   for (var j = 0; j < numberOfComments; j++) {
     var message = '';
     var numberOfPhrases = getRandomNumber(MIN_PHRASES, MAX_PHRASES + 1);
+    var nameIndex = getRandomNumber(0, rawData.names.length);
 
     for (var i = 0; i < numberOfPhrases; i++) {
       var separator = i > 0 ? ' ' : '';
 
-      message += separator + photoData.comments[getRandomNumber(0, photoData.comments.length)];
+      message += separator + rawData.comments[getRandomNumber(0, rawData.comments.length)];
     }
 
     comments.push({
-      message: message
+      message: message,
+      avatar: AVATAR_FOLDER + avatars[j] + AVATER_EXT,
+      name: rawData.names[nameIndex]
     });
   }
 
   return comments;
 };
+
+// Генирируем данные для фотографий и присваиваем их переменной
+var photoGeneratedData = setPhotoElementData(NUMBER_OF_PHOTOS);
 
 // Получаем шаблон
 var pictureElementTemplate = document.querySelector('#picture').content.querySelector('.picture');
@@ -125,8 +146,60 @@ var createPictureFragment = function (pictures) {
 var generatePictures = function () {
   var pictureListElements = document.querySelector('.pictures');
 
-  pictureListElements.appendChild(createPictureFragment(setPhotoElementData(NUMBER_OF_PHOTOS)));
+  pictureListElements.appendChild(createPictureFragment(photoGeneratedData));
+};
+
+// Получаем элементы полноэкранного изображения
+var bigPicture = document.querySelector('.big-picture');
+var commentElements = bigPicture.querySelector('.social__comments');
+var commentElement = commentElements.querySelector('.social__comment');
+
+// Отрисовка полноэкранного изображения
+var initBigPicture = function () {
+  setBigPicture(photoGeneratedData[0]);
+
+  bigPicture.classList.remove('hidden'); // Убираем класс hidden
+  bigPicture.querySelector('.social__comment-count').classList.add('visually-hidden'); // Скрываем счётчик комментариев
+  bigPicture.querySelector('.social__comments-loader').classList.add('visually-hidden'); // Скрываем кнопку "показать больше"
+};
+
+// Задаем информацию полноэкранному изображению
+var setBigPicture = function (picture) {
+  var fragment = document.createDocumentFragment();
+
+  bigPicture.querySelector('img').src = picture.url; // Указываем путь к фото
+  bigPicture.querySelector('.likes-count').textContent = picture.likes; // Показываем количество лайков
+  bigPicture.querySelector('.comments-count').textContent = picture.comments.length; // Показываем количество комментариев
+  bigPicture.querySelector('.social__caption').textContent = picture.description; // Показываем описание к фото
+
+  picture.comments.forEach(function (item) { // Для каждого комментария создаем фрагмент и заполняем его информацией из массива
+    fragment.appendChild(setCommentData(item));
+  });
+
+  clearElements(commentElements); // Очищаем ненужные комментарии
+  commentElements.appendChild(fragment); // Вставляем в разметку новые комментарии
+};
+
+// Удаляем ненужные DOM элементы
+var clearElements = function (element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+};
+
+// Заполняем комментарии из массива
+var setCommentData = function (comment) {
+  var element = commentElement.cloneNode(true);
+
+  element.querySelector('.social__picture').src = comment.avatar;
+  element.querySelector('.social__picture').alt = comment.name;
+  element.querySelector('.social__text').textContent = comment.message;
+
+  return element;
 };
 
 // Инициализация
 generatePictures();
+initBigPicture();
+
+
